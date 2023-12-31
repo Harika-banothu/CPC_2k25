ae0a3467edc1ac889415cc1586dc75dc745e52f5
Harika <105665785+Harika-banothu@users.noreply.github.com>
2023-12-31 13:06:19 +0530

    Create 8.cpp

---
+++ DSA_SHEET/Bit_Manipulation/8.cpp
@@ -0,0 +1,13 @@
+//Add two numbers without using arithmetic operators
+
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while(b!=0){
+            int carry = a&b;
+            a = a^b;
+            b = carry<<1;
+        }
+        return a;
+    }
+};
