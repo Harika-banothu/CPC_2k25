5fb0f5dd545efa6d21abb134280c2e1469276557
Harika <105665785+Harika-banothu@users.noreply.github.com>
2023-12-31 13:33:08 +0530

    Create 10.cpp

---
+++ DSA_SHEET/Bit_Manipulation/10.cpp
@@ -0,0 +1,10 @@
+//Swap every consecutive odd and even positioned bit in a number
+unsigned int swapBits(unsigned int n)
+    {
+    	// Your code here
+    	unsigned int even = n & 0xAAAAAAAA;
+    	unsigned int odd = n & 0x55555555;
+    	even >>= 1;
+    	odd <<= 1;
+    	return (even | odd);
+    }
