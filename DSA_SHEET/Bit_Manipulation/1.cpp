1ee70b84f58f34c14abfe73c883000fdee6588c8
Harika <105665785+Harika-banothu@users.noreply.github.com>
2023-12-31 11:52:20 +0530

    Update 1.cpp

--- DSA_SHEET/Bit_Manipulation/1.cpp
+++ DSA_SHEET/Bit_Manipulation/1.cpp
@@ -1,1 +1,10 @@
-
+//Check if a given number is a power of 2
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if (n <= 0)
+            return false;
+        
+        return (n & (n - 1)) == 0;  
+    }
+};
