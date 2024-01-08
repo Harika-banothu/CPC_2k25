f4c10269dd5e2f2609bda6c5663b4996d9a14236
Harika <105665785+Harika-banothu@users.noreply.github.com>
2024-01-08 09:36:11 +0530

    Create nxthigh_permtatn.cpp

---
+++ DSA_SHEET/Arrays/nxthigh_permtatn.cpp
@@ -0,0 +1,9 @@
+//find the next higher permutation of the given number as an array of digits. If such a number
+//doesn’t exist, return -1.
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(),nums.end());
+       
+    }
+};
