b14a4d31f55bd965075f7db92754d83178a1edc3
Harika <105665785+Harika-banothu@users.noreply.github.com>
2024-01-08 09:27:00 +0530

    Create numocc_oddtimes.cpp

---
+++ DSA_SHEET/Arrays/numocc_oddtimes.cpp
@@ -0,0 +1,11 @@
+//find number occuring odd no.of times
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int res = 0;
+        for(int i=0;i<nums.size();i++){
+            res=res^nums[i];
+        }
+        return res;
+    }
+};
